b2a437c0595fdffb2e6a8084dd5450cc95e12465
naoise collins <naoisecollins@gmail.com>
2023-11-09 12:56:32 +0000

    Added The basic game Loop together

--- Examples/Basic Example/HaloInfinity+2/main.js
+++ Examples/Basic Example/HaloInfinity+2/main.js
@@ -1,89 +1,50 @@
-   //import{hello} from "../main.js";
-let num = 25; 
-let str = "Hello world";
-let isAvailable = true; 
-let notDefined;
-let isNull = null; 
-let obj = {firstName: "John", lastName: "Smith"};
+const player = document.getElementById("player");
+const enemy = document.getElementById("enemy");
+const gameArea = document.getElementById("game-container");
 
-let condition = true; 
-if(condition) {
-    console.log("Condition is true");
-} else {
-    console.log("Condition is false");
+const playerWidth = player.offsetWidth;
+const playerHeight = player.offsetHeight;
+const gameWidth = gameArea.offsetWidth; 
+const gameHeight = gameArea.offsetHeight; 
+
+let playerX = 0;
+let playerY = 75; 
+let enemyX = Math.random() * (gameWidth - playerWidth);
+let enemyY = Math.random() * (gameHeight - playerHeight);
+
+let gameOver = false; 
+let keys = {};
+
+function gameLoop(){
+handlePlayerMovement();
+moveEnemyTowardsPlayer();
+updatePlayerPosition();
+updateEnemyPosition();
+checkColllisions();
+gameOver = true; 
+
+if(!gameOver){
+    requestAnimationFrame(gameLoop);
+}else{
+    endGame();
+}
 }
 
-for(let i = 0; i<5; i++){
-    console.log(i);
-}
-//this is a while loop 
-let j = 0; 
-while(j<5){
-    console.log(j);
-    j++;
+function endGame(){
+    alert("Game Over");
+    resetGame();
 }
 
-function greet(name){
-    console.log("hello " + name)
+function resetGame()
+{
+    playerX = 0;
+    playerY = 75; 
+    enemyX = Math.random() * (gameWidth - playerWidth);
+    enemyY = Math.random() * (gameHeight - playerHeight);
+    keys = {};
+    player.style.opacity = 1; 
+    gameOver = false; 
+    requestAnimationFrame(gameLoop);       
 }
 
-greet("Alice");
-
-let fruits = [];
-fruits[0] = "apple";
-fruits[1] = "pear";
-fruits[2] = "strawberry";
-console.log(fruits[1]);
-
-let person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-};
-console.log(person.firstName);
-
-var varVariable = "I'm a var variable";
-let letVariable = "I'm a let variable";
-const constVariable = "I'm a const variable";
-
-varVariable = 25;
-letVariable = "Updated let Variable";
-//constVariable = "Updated const Variable";
-
-console.log(varVariable);
-console.log(letVariable);
-console.log(constVariable);
-
-class Car{
-    constructor(brand,model){
-        this.brand = brand;
-        this.model = model;
-    }
-    displayCar(){
-        return this.brand + " " + this.model;
-    }
-}
-let myCar = new Car("Toyota", "Corolla");
-console.log(myCar.displayCar());
-
-class Model extends Car {
-    constructor(brand, model, year){
-        super(brand, model);
-        this.year = year;  
-    }
-    displayModel(){
-        return this.displayCar() + " " + this.year;
-    }
-}
-    let myModel = new Model("Toyota", "Corolla", "2023");
-    console.log(myModel.displayModel());
-
-    const arr = [1,2,3,4,5];
-    const squares = arr.map((x) => x*x);
-    console.log(squares);
-
-    //template literals 
-    let nameGirl = "Alice";
-    console.log(`Hello ${nameGirl}!`); 
-
-    //import{hello} from "../main.js";
+requestAnimationFrame(gameLoop);
