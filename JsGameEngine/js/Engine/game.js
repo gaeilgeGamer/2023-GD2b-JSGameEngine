37dbb1ef853099b3af046a713ef079e934da7aa2
naoise collins <naoisecollins@gmail.com>
2023-11-23 11:41:38 +0000

    base classes created

--- JsGameEngine/js/Engine/game.js
+++ JsGameEngine/js/Engine/game.js
@@ -11,4 +11,56 @@ class Game{
         window.addEventListener("resize",()=> this.resizeCanvas());
         //this.camera = new Camera(null, this.canvas.width, this.canvas.height);
     }
+    resizeCanvas(){
+        this.canvas.width = window.innerWidth -50;
+        this.canvas.height = window.innerHeight -50;
+    }
+    start(){
+        this.isRunning = true; 
+        requestAnimationFrame((timestamp)=> this.gameLoop(timestamp));
+    }
+    gameLoop(currentFrameTime){
+        this.deltaTime = (currentFrameTime - this.lastFrameTime)/1000;
+        this.lastFrameTime = currentFrameTime; 
+
+    this.update();
+    //this.camera.update();
+    this.draw();
+    requestAnimationFrame((timestamp)=> this.gameLoop(timestamp));
 }
+    update(){
+        for(const gameObject of this.gameObjects){
+            gameObject.update(this.deltaTime);
+        }
+        this.gameObjects = 
+        this.gameObjects.filter(obj => !this.gameObjectsToRemove.includes(obj));
+        this.gameObjectsToRemove=[];
+    }
+    draw(){
+        this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+        this.ctx.save();
+        //this.ctx.translate(-camera.x, -this.camera.y);
+
+        for(const gameObject of this.gameObjects){
+            gameObject.draw(this.ctx);
+        }
+        this.ctx.restore();
+    }
+    addGameObject(gameObject){
+        gameObject.game = this; 
+        this.gameObjects.push(gameObject);
+    }
+    removeGameObject(gameObject){
+        this.gameObjectsToRemove.push(gameObject);
+    }
+    reset(){
+        this.isRunning = false;
+        for(const gameObject of this.gameObjects){
+            if(gameObject.reset){
+                gameObject.reset();
+            }
+        } 
+        this.start();
+    }
+}
+export default Game;
