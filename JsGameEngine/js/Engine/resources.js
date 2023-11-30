982554796f9451b9852bda291d47d5cf67a8db6b
naoise collins <naoisecollins@gmail.com>
2023-11-30 10:17:57 +0000

    added resources, ui, particlesystem

---
+++ JsGameEngine/js/Engine/resources.js
@@ -0,0 +1,14 @@
+const Images = {
+    player: new Image(),
+    enemy: new Image(),
+};
+
+const AudioFiles = {
+    jump: "....",
+    collect: "...",
+};
+
+Images.player.src = "";
+Images.enemy.src = "";
+
+export {Images, AudioFiles};
